9a4dc6799c6371e490599ac23341db8d11eedf2e
shaanxizhuzhe@163.com <shaanxizhuzhe@163.com>
2015-06-01 16:05:36 +0800

    update

--- src/com/snicesoft/http/HttpReq.java
+++ src/com/snicesoft/http/HttpReq.java
@@ -10,11 +10,13 @@ import android.text.TextUtils;
 
 import com.alibaba.fastjson.JSON;
 import com.lidroid.xutils.HttpUtils;
+import com.lidroid.xutils.exception.HttpException;
 import com.lidroid.xutils.http.HttpHandler;
 import com.lidroid.xutils.http.HttpHandler.State;
 import com.lidroid.xutils.http.RequestParams;
 import com.lidroid.xutils.http.callback.RequestCallBack;
 import com.lidroid.xutils.http.client.HttpRequest.HttpMethod;
+import com.snicesoft.service.ServiceCallback;
 
 @SuppressWarnings("deprecation")
 public class HttpReq {
@@ -27,19 +29,21 @@ public class HttpReq {
 		this.httpUtils.configRequestThreadPoolSize(10);
 		this.httpUtils.configResponseTextCharset(Charset.UTF_8);
 	}
-	
+
 	public HttpUtils httpUtils() {
 		return httpUtils;
 	}
+
 	public void GET(String api, Map<String, Object> params,
-			RequestCallBack<String> callBack) {
+			HttpCallback callBack) {
 		RequestParams rp = new RequestParams();
 		if (params != null)
 			for (String key : params.keySet()) {
 				rp.addQueryStringParameter(key, params.get(key).toString());
 			}
 		cancelHandler(httpHandlerMap.get(api));
-		HttpHandler<String> hh = httpUtils.send(HttpMethod.GET, api, rp, callBack);
+		HttpHandler<String> hh = httpUtils.send(HttpMethod.GET, api, rp,
+				callBack);
 		httpHandlerMap.put(api, hh);
 	}
 
@@ -53,7 +57,7 @@ public class HttpReq {
 	 * @param callBack
 	 */
 	public void POST(String api, Map<String, Object> params,
-			RequestCallBack<String> callBack) {
+			HttpCallback callBack) {
 		RequestParams rp = new RequestParams();
 		if (params != null)
 			for (String key : params.keySet()) {
@@ -64,7 +68,8 @@ public class HttpReq {
 				}
 			}
 		cancelHandler(httpHandlerMap.get(api));
-		HttpHandler<String> hh = httpUtils.send(HttpMethod.POST, api, rp, callBack);
+		HttpHandler<String> hh = httpUtils.send(HttpMethod.POST, api, rp,
+				callBack);
 		httpHandlerMap.put(api, hh);
 	}
 
@@ -79,7 +84,7 @@ public class HttpReq {
 	 * @param callBack
 	 */
 	public void POST(String api, String requestBody, String contentType,
-			RequestCallBack<String> callBack) {
+			HttpCallback callBack) {
 		RequestParams params = new RequestParams();
 		try {
 			if (!TextUtils.isEmpty(contentType))
@@ -90,7 +95,8 @@ public class HttpReq {
 			e.printStackTrace();
 		}
 		cancelHandler(httpHandlerMap.get(api));
-		HttpHandler<String> hh = httpUtils.send(HttpMethod.POST, api, params, callBack);
+		HttpHandler<String> hh = httpUtils.send(HttpMethod.POST, api, params,
+				callBack);
 		httpHandlerMap.put(api, hh);
 	}
 
@@ -105,7 +111,7 @@ public class HttpReq {
 	 *            请求回调函数
 	 */
 	public void postJson(String api, Map<String, Object> params,
-			RequestCallBack<String> callBack) {
+			HttpCallback callBack) {
 		String requestBody = JSON.toJSONString(params);
 		POST(api, requestBody, ContentType.JSON, callBack);
 	}
@@ -115,6 +121,32 @@ public class HttpReq {
 				&& handler.getState() != State.SUCCESS
 				&& handler.getState() != State.CANCELLED) {
 			handler.cancel();
+		}
+	}
+
+	public static abstract class HttpCallback extends RequestCallBack<String> {
+		private ServiceCallback<?> callback;
+
+		public HttpCallback(ServiceCallback<?> callback) {
+			this.callback = callback;
+		}
+
+		@Override
+		public void onStart() {
+			if (callback != null)
+				callback.onStart();
+		}
+
+		@Override
+		public void onLoading(long total, long current, boolean isUploading) {
+			if (callback != null)
+				callback.onLoading(total, current, isUploading);
+		}
+
+		@Override
+		public void onFailure(HttpException error, String msg) {
+			if (callback != null)
+				callback.onFailure(error, msg);
 		}
 	}
 }
